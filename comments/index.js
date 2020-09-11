22b9dc8290ffdfd3a29a5bde374d7c548376c5cc
Sushant Gupta <imsushant25@gmail.com>
2020-09-11 19:06:36 +0000

    dockerfiles added

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const cors = require('cors')
 const app = express()
+const axios = require('axios');
+
 app.use(bodyParser.json())
 app.use(cors())
 const { randomBytes } = require("crypto")
@@ -11,17 +13,46 @@ app.get("/posts/:id/comments", (req,res) => {
     res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post("/posts/:id/comments", (req,res) => {
+app.post("/posts/:id/comments",async (req,res) => {
     const commentId = randomBytes(4).toString("hex")
     const postId = req.params.id
     const {content} = req.body
     const comments = commentsByPostId[postId] || [];
 
-    comments.push({ id : commentId, content});
+    comments.push({ id : commentId, content, status : 'pending'});
     commentsByPostId[postId] = comments
+    await axios.post('http://localhost:4005/events', {
+        type : 'CommentCreated',
+        data : {
+            id : commentId,
+            content,
+            postId,
+            status:'pending'
+        }
+    })
     res.statusCode(201).send(comments)
 })
-
+app.post('/events', async (req,res) => {
+    const {type, data } = req.body
+    if(type === 'CommentModerated') {
+        const {postId, id, status} = data
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        })
+        comment.status = status;
+        await axios.post('http://localhost:4005', {
+            type : 'CommentUpdated',
+            data : {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
+    res.send({})
+})
 app.listen(4001, () => {
     console.log("Listening on post 4001");
 })
--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const bodyParser = require("body-parser")
-const cors = require('cors')
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-const { randomBytes } = require("crypto")
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const { randomBytes } = require("crypto");
+const axios = require('axios');
 const posts = {}
 app.get('/', (req,res) => {
     res.send("Hello")
@@ -13,15 +14,24 @@ app.get('/posts', (req,res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req,res) => {
+app.post('/posts',async (req,res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body
     posts[id] = {
         id,title
-    }
+    };
+    await axios.post('http:localhost:4005/events', {
+        type : 'PostCreated',
+        data : {
+            ...posts[id]
+        }
+    })
     res.status(201).send(posts[id]);
 })
 
+app.post('/events', (req,res) => {
+    res.send({})
+})
 app.listen(4000 ,() => {
     console.log("Listening on port 4000")
 })
